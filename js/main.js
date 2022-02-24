2740438372803d962549c33795aff6112dbb0f65
Литвиненко Юрий <litvinenko-don@ukr.net>
2022-02-24 12:50:20 +0300

    fix: changed main.js

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ function rndInt(min, max) {
   // случайное число от min до max+1
   if (min < 0) {
     min = 0;
-  };
+  }
 
   if (min >= max) {
     return null;
-  };
+  }
 
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
-// for (let i = 0; i < 10; i++) {
-//   console.log(rndInt(2, 5));
-// }
+for (let i = 0; i < 10; i++) {
+  console.log(rndInt(2, 5));
+}
 
 /***** */
 function rndFloatPoint(min, max, n) {
@@ -22,21 +22,21 @@ function rndFloatPoint(min, max, n) {
 
   if (min < 0) {
     min = 0;
-  };
+  }
 
   if (min >= max) {
     return null;
-  };
+  }
 
   if (n < 0) {
     n = 0;
-  };
+  }
 
   let rand = Math.random() * (max - min) + min;
   rand = rand.toFixed(n);
   return rand;
 }
 
-// for (let i = 0; i < 10; i++) {
-//   console.log(rndFloatPoint(1.1, 5.5, 2));
-// }
+for (let i = 0; i < 10; i++) {
+  console.log(rndFloatPoint(1.1, 5.5, 2));
+}
